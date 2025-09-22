84a397eb82d43faeadf193b53356595cf64eb266
unknown <ajincraju@gmail.com>
2025-09-22 09:26:02 +0530

    Modified Home Page

--- app/(root)/home/page.jsx
+++ app/(root)/home/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1>FinGuard</h1>
+      <p>Upload your CSV sheet and make us analyze your spending habits!</p>
+    </div>
   )
 }
 
