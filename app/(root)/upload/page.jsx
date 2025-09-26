5280c2e4b626116b368c8e09c6c9831b4e4f80e2
unknown <ajincraju@gmail.com>
2025-09-26 08:54:47 +0530

    Modified the Uploads Page

--- app/(root)/upload/page.jsx
+++ app/(root)/upload/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <h1>Upload Page</h1>
+      <p>Upload your CSV files here!</p>
+    </div>
   )
 }
 
