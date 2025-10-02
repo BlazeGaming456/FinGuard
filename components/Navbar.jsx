5c73c6f460497abc92bd5d624db83de29dd5c5c9
unknown <ajincraju@gmail.com>
2025-10-02 09:43:26 +0530

    Modified the Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,6 +4,12 @@ const Navbar = () => {
   return (
     <div>
         <h1>FinGuard</h1>
+        <ul>
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li>About</li>
+          <li>Account</li>
+        </ul>
     </div>
   )
 }
