f228bb5314ca64432665af2b80377799737461ce
unknown <ajincraju@gmail.com>
2026-02-21 01:17:25 +0530

    feat(validation): Added basic CSV validation and formatting

--- app/(root)/upload/page.jsx
+++ app/(root)/upload/page.jsx
@@ -19,10 +19,61 @@ const page = () => {
     })
   }
 
+  const headerMap = {
+    date: ["date", "transaction date", "value date", "txn date"],
+    description: ["description", "narration", "remarks", "details"],
+    amount: ["amount"],
+    type: ["type", "transaction type"]
+  }
+
+  const normalizeData = (data) => {
+    return data
+    .trim()
+    .toLowerCase()
+    .replace(/\(.*?\)/g, "") // remove (INR)
+    .replace(/[^a-z0-9 ]/g, "") // remove special chars
+    .replace(/\s+/g, " "); // normalize spaces
+  }
+
+  const mapHeaders = (headers) => {
+    const mappedHeaders = {};
+
+    for (let rawHeader of headers) {
+      const normalized = normalizeData(rawHeader);
+
+      for (let key in headerMap) {
+        if (headerMap[key].includes(normalized)) {
+          mappedHeaders[rawHeader] = key;
+        }
+      }
+    }
+    return mappedHeaders;
+  }
+
+  const validateHeaders = (mappedHeaders) => {
+    if (!mappedHeaders.date || !mappedHeaders.description || !mappedHeaders.amount || !mappedHeaders.type) {
+      alert("CSV file is missing required headers. Please ensure it includes date, description, amount, and type.");
+      return false;
+    }
+  }
+
   const handleFileChange = async (e) => {
     //Directly use the file from the event target instead of selectedFile, as it takes time for the state to update and it will not do anything
     const file = e.target.files[0];
     setSelectedFile(file)
+    // File Validation
+    if (file.size() > 5 * 1024 * 1024) { // 5MB limit
+      alert ('File size exceeds 5MB limit. Please upload a smaller file.');
+      return;
+    }
+    if (file.type !== 'text/csv') {
+      alert('Invalid file type. Please upload a CSV file.');
+      return;
+    }
+    if (file.size() === 0) {
+      alert('File is empty. Please upload a non-empty CSV file.');
+      return;
+    }
     if (file) {
       console.log("In the process")
       Papa.parse(file, {
@@ -31,7 +82,13 @@ const page = () => {
         complete: async results => {
           setParsedData(results.data)
           console.log(results.data)
-          await saveTransactions(results.data);
+          
+          const csvHeaders = Object.keys(results.data[0]);
+          const mappedHeaders = mapHeaders(csvHeaders);
+          if (validateHeaders(mappedHeaders) === true) {
+            console.log ('Headers are valid, proceeding to save transactions');
+            await saveTransactions(results.data);
+          }
         }
       })
       
